32a083b6a9f2288957d668a98c95c5014a6279ac
aki <ahsi.aki@gmail.com>
2022-07-26 09:16:36 +0800

    update img

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2 className="text-3xl font-bold underline text-indigo-400">
+      <h2 className="text-3xl font-bold underline bg-sky-500 text-green-100">
         Hello world!
       </h2>
     </div>
