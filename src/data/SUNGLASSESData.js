592dddbb20979cbd25c35512c02b33deb3423b32
aki <ahsi.aki@gmail.com>
2022-07-26 23:42:31 +0800

    data setting

---
+++ src/data/SUNGLASSESData.js
@@ -0,0 +1,36 @@
+const data  =[
+  { id:"7",
+    icon:"product-7.png",
+    title:"BJ41600S",
+    price:"3490"
+  },
+  { id:"8",
+  icon:"product-8.png",
+  title:"BJ41600S",
+  price:"3490"
+  },
+  { id:"9",
+    icon:"product-9.png",
+    title:"BJ41600S",
+    price:"3490"
+  },
+  { id:"10",
+  icon:"product-10.png",
+  title:"BJ41600S",
+  price:"3490"
+  },
+  { id:"11",
+    icon:"product-11.png",
+    title:"BJ41600S",
+    price:"3490"
+  },
+  { id:"12",
+  icon:"product-12.png",
+  title:"BJ41600S",
+  price:"3490"
+  },
+
+  
+]
+
+export default data; 
---
+++ src/data/StoresCardData.js
@@ -0,0 +1,40 @@
+const data  =[
+  { id:"1",
+    img:"store-1.png",
+    title:"台北中山旗艦店",
+    tel:"(02)000-1234",
+    openTime:"10:00-21:00",
+    add:"台北市中山區南京東路25巷2-1號"
+  },
+  { id:"2",
+  img:"store-2.png",
+  title:"台北綠園店",
+  tel:"(02)000-2345",
+  openTime:"10:00-21:00",
+  add:"台北市中正區復興南路 132-1 號"
+  },
+  { id:"3",
+  img:"store-3.png",
+  title:"台中清水旗艦店",
+  tel:"(02)000-1234",
+  openTime:"10:00-21:00",
+  add:"台中市清水區經南一路 23 號 8 樓"
+  },
+  { id:"4",
+  img:"store-4.png",
+  title:"高雄中正形象店",
+  tel:"(07)000-2345",
+  openTime:"10:00-21:00",
+  add:"高雄市苓雅區中正路 38 號 12 樓"
+  },
+  { id:"5",
+  img:"store-5.png",
+  title:"高雄夢時代店",
+  tel:"(07)000-1234",
+  openTime:"10:00-21:00",
+  add:"高雄市前鎮區中華一路 63 號 6 樓"
+  }
+  
+]
+
+export default data; 
