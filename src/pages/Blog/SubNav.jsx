71481873d879158eb70e175be36dffe2affe37c7
aki <ahsi.aki@gmail.com>
2022-08-08 22:19:47 +0800

    etarget set

--- src/pages/Blog/SubNav.jsx
+++ src/pages/Blog/SubNav.jsx
@@ -6,11 +6,11 @@ const SubNav = () => {
   return (
     <nav className="hidden md:block text-white bg-wine-600 leading-normal text-[20px]">
     <div className='my-container flex py-3 justify-between'>
-      <ul className='flex gap-12'>
-        <li> <a href="/" onClick={(e)=>e.preventDefault()}>最新消息</a> </li>
-        <li> <a href="/" onClick={(e)=>e.preventDefault()}>特別企劃</a> </li>
-        <li> <a href="/" onClick={(e)=>e.preventDefault()}>新品上市</a> </li>
-        <li> <a href="/" onClick={(e)=>e.preventDefault()}>鏡框小知識</a> </li>
+      <ul className='flex gap-12' onClick={(e)=>e.preventDefault()}>
+        <li> <a href="!#">最新消息</a> </li>
+        <li> <a href="!#">特別企劃</a> </li>
+        <li> <a href="!#">新品上市</a> </li>
+        <li> <a href="!#">鏡框小知識</a> </li>
       </ul>
       <Icon
             className="w-7 inline-block"
